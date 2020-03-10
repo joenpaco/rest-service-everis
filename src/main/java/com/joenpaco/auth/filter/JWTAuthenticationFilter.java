19569f484f9f21191b91b7bb8f0af94692c8f53d
Jose Parico <joenpaco@gmail.com>
2020-03-10 09:52:33 -0300

    Elimina comentarios

--- src/main/java/com/joenpaco/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/joenpaco/auth/filter/JWTAuthenticationFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.FilterChain;
